f59789071de276eb4147264d0f11d81195ceebe7
SW <aliwo@naver.com>
2024-10-16 15:02:55 +0900

    do exists query()

--- tabom/services/like_service.py
+++ tabom/services/like_service.py
@@ -2,4 +2,6 @@ from tabom.models import Like
 
 
 def do_like(user_id: int, article_id: int) -> Like:
+    if Like.objects.filter(user_id=user_id, article_id=article_id).exists():
+        raise Exception
     return Like.objects.create(user_id=user_id, article_id=article_id)
